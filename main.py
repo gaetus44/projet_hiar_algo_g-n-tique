9726ae83ab00b78d8783323abf3b08c22d5c2c12
TobiasTwo <61496431+TobiasTwo@users.noreply.github.com>
2025-10-22 18:02:59 +0200

    Enhance main.py with comments for clarity
    Added comments to explain the target binary string and genetic algorithm initialization.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from simple_genetic_algorithm import SimpleGeneticAlgorithm
 
 if __name__ == "__main__":
-    solution = "1010101010101010101010101010101010101010101010101010101010101010" #1010101010101010101010101010101010101010101010101010101010101010
+    # Définition de la chaîne binaire cible (la solution) que l'algorithme doit trouver.
+    solution = "1010101010101010101010101010101010101010101010101010101010101010"
+    
+    # Initialisation de l'algorithme génétique.
+    # On passe la solution cible et le type de sélection (ici, "tournament").
+    # D'autres options comme "roulette" sont possibles (comme indiqué dans le constructeur de SimpleGeneticAlgorithm).
     ga = SimpleGeneticAlgorithm(solution, selection_type="tournament") # tournament - roulette
+
+    # Lancement de l'algorithme avec une population de 50 individus.
     ga.run_algorithm(50)
 
